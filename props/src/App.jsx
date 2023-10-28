ba6221cd3655a91d13f2053e111f95227c6bfd0c
fiyaznkp <fiyaznkp@gmail.com>
2023-10-28 17:46:34 +0530

    first commit

---
+++ props/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Sample from './sample'
+
+function App() {
+
+  return(
+    <div>
+    <Sample name = {"fiyaz"}/>
+    </div>
+  )
+}
+
+export default App
---
+++ props/src/Sample.jsx
@@ -0,0 +1,10 @@
+const Sample= (props) =>{
+   
+    return(
+        <div>
+
+        {props.name}
+        </div> 
+        )
+}
+export default Sample
